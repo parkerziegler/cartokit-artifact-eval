a2fea03271f1fd6ed88631780f8d1fb200b5208f
Parker Ziegler <parkerellisziegler@gmail.com>
2025-03-18 12:40:00 -0700

    fix: Remove gpu.spec.ts.

--- packages/cartokit/tests/gpu.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test } from '@playwright/test';
-
-test('GPU hardware acceleration', async ({ page }) => {
-  await page.goto('chrome://gpu');
-  await page.screenshot({ path: 'gpu.png' });
-});
